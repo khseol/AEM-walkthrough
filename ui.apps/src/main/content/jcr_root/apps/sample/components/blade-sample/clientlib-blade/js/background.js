0af78f5d19e4eae34143a11b77a08cd04faa8ad0
Kathy Seol <kseol@skillstorm.com>
2021-08-12 17:43:22 -0400

    edieted the HTL

---
+++ ui.apps/src/main/content/jcr_root/apps/sample/components/blade-sample/clientlib-blade/js/background.js
@@ -0,0 +1,6 @@
+//the function that will set the background image and size of the component.
+use(function(){
+    var background = document.getElementsByClassName("container");
+   //var imageList = document.getElementsByClassName();
+    imgPath.style.backgroundImage = "url(${currentImgPath})";
+})
