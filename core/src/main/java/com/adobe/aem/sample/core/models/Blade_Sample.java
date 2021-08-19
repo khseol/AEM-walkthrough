d59965d8f6d8b62125d53a432e6c8186884048a3
Kathy Seol <kseol@skillstorm.com>
2021-08-19 09:29:39 -0400

    added a jackson exporter to the blade component sling mdel

--- core/src/main/java/com/adobe/aem/sample/core/models/Blade_Sample.java
+++ core/src/main/java/com/adobe/aem/sample/core/models/Blade_Sample.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
@@ -11,11 +12,19 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 /**
  * 
  * @author kathyhseol
- *	to complications with the multifields in granite and coral, the content.xml structure for the dialog
+ *	due to complications with the multifields in granite and coral, the content.xml structure for the dialog
  *will no longer contain a multifield.
+ *
+ * 08/19/21
+ * the sling model below will be using a sling model exporter using Jackson exporter for the purpose of developing in AEM.
  */
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL) //declared the class a SLING MODEL
+@Model(adaptables = Resource.class, 
+		resourceType = Blade_Sample.RESOURCE_TYPE,
+		defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL) //declared the class a SLING MODEL
+@Exporter(name = "jackson", extensions = "json")
 public class Blade_Sample {
+	protected final static String RESOURCE_TYPE = "sample/components/blade-sample"; //this defines the component or the resource
+	
 
 	@ValueMapValue
 	private String bladetitle;
